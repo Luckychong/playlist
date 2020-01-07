4d63ff3fb8737998c40da4c95277e4472912818e
Luckychong <lucky0212@yahoo.com>
2020-01-07 02:26:22 +0000

    almost finish Day 1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let songs = ["Dope","The Truth Untold","Dimple","Jealous"]
+songs.forEach(function(songs) {
+    $("#songs").append(`<p> I want to listen to "${songs}" </p>`)
+})
+let songArtists = ["BTS", "Chris Brown"]
+songArtists.forEach(function(songs) {
+    $("#songartists").append(`<p> The artists are "${`)
+}
