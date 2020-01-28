9ef3b0c2ffa6e7f21ba251b2d261dcf8db821328
Luckychong <lucky0212@yahoo.com>
2020-01-28 02:26:46 +0000

    objects

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-let songs = ["Dope","The Truth Untold","Dimple","Jealous"]
+let songdata = {
+    songTitle: "The Truth Untold",
+    songArtist: "BTS",
+    songImage: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjJxsyCmqXnAhVEJTQIHeboAbAQjRx6BAgBEAQ&url=http%3A%2F%2Ft0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcSEC1ihcXegp1-DOra8NPcBToXGs_uMsh2Z5L7cxfpM_KBDHSWv&psig=AOvVaw0WBjJ4orNwgs5J-h6AVp1k&ust=1580263390421573",
+    songLength: "",
+    songLink: "https://www.youtube.com/watch?v=eRNJGMZ_zpw",
+
+}
+let songs = [songdata, ]
+$("#add").click(function() {
+    songs.forEach (function(song){
+    $("#songs").append("<p> songTitle: "+song["songTitle"]+"</p>");
+$("#songs").append("<p> songArtists: "+song["songArtist"]+"</p>");
+$("#songs").append("<p> songImage: "+song["songImage"]+"</p>");
+$("#songs").append("<p> songLength: "+song["songLength"]+"</p>");
+$("#songs").append("<p> songLink: "+song["songLink"]+"</p>");
+    })
+})
 songs.forEach(function(songs) {
     $("#songs").append(`<p> I want to listen to "${songs}" </p>`)
 })
@@ -6,7 +23,7 @@ let songArtists = ["BTS", "Chris Brown"]
 songArtists.forEach(function(songs) {
     $("#songartists").append(`<p> The artists are "${songs}"`)
 })
-let songImage = []
+let songImage = ["https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjJxsyCmqXnAhVEJTQIHeboAbAQjRx6BAgBEAQ&url=http%3A%2F%2Ft0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcSEC1ihcXegp1-DOra8NPcBToXGs_uMsh2Z5L7cxfpM_KBDHSWv&psig=AOvVaw0WBjJ4orNwgs5J-h6AVp1k&ust=1580263390421573"]
 songImage.forEach(function(songs) { 
     $("#songImage").append(`<p> This is how the artists look like" "${songs}" </p>`)
 })
@@ -26,3 +43,9 @@ songs.forEach(function(song) {
     $("#songs").append(`<p> I want to listen to "${song}" </p>`)
 })
 }) 
+let songs
+let songArtists
+let songImage
+let songLink
+let songLength
+
